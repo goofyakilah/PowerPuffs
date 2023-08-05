48b8d87c0c87ec11d211d522606f788c494b1c7f
Grace <dug29contacts@gmail.com>
2023-08-05 16:58:55 -0400

    made my own progect

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class MecanumDriveGrace extends OpMode {
+
+    DcMotor RFMotor;
+    DcMotor LFMotor;
+    DcMotor RBMotor;
+    DcMotor LBMotor;
+
+
+
+
+
+
+}
