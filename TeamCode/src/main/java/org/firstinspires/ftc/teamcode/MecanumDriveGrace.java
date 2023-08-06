1379a9f69deb5962c471d8c29638a5c91bdf96fd
Grace <dug29contacts@gmail.com>
2023-08-05 20:59:06 -0400

    renamed cars project for her cuz she told me to

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
@@ -6,12 +6,48 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class MecanumDriveGrace extends OpMode {
-
     DcMotor RFMotor;
     DcMotor LFMotor;
     DcMotor RBMotor;
     DcMotor LBMotor;
 
+    public void moveDriveTrain(){
+
+        double vertical= gamepad1.left_stick_y;
+        double horizontal= gamepad1.left_stick_x;
+        double pivot = gamepad1.right_stick_x;
+
+        RFMotor.setPower(pivot+);
+        RBMotor.setPower(pivot+ );
+        @Override
+        public void init() {
+            RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+            LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+            RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+            LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+        }
+
+        @Override
+        public void init_loop(){
+
+        }
+
+        @Override
+        public void loop(){
+
+        }
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
 
 
 
