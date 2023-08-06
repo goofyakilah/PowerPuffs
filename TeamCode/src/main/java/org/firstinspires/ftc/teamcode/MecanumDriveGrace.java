b10ba4cd2d974d2417f5d93f8f036d4aa18d9df3
Grace <dug29contacts@gmail.com>
2023-08-05 22:46:00 -0400

    made my program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MecanumDriveGrace extends OpMode {
@@ -11,32 +12,41 @@ public class MecanumDriveGrace extends OpMode {
     DcMotor RBMotor;
     DcMotor LBMotor;
 
-    public void moveDriveTrain(){
+    public void moveDriveTrain() {
+        double vertical = 0;
+        double horizontal = 0;
+        double pivot = 0;
 
-        double vertical= gamepad1.left_stick_y;
-        double horizontal= gamepad1.left_stick_x;
-        double pivot = gamepad1.right_stick_x;
+        vertical = -gamepad1.left_stick_y;
+        horizontal = gamepad1.left_stick_x;
+        pivot = gamepad1.right_stick_x;
 
-        RFMotor.setPower(pivot+);
-        RBMotor.setPower(pivot+ );
-        @Override
-        public void init() {
-            RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
-            LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
-            RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
-            LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
-        }
-
-        @Override
-        public void init_loop(){
-
-        }
-
-        @Override
-        public void loop(){
-
-        }
+        RFMotor.setPower(pivot + (-vertical + horizontal));
+        RBMotor.setPower(pivot + (-vertical - horizontal));
+        LFMotor.setPower(pivot + (-vertical - horizontal));
+        RFMotor.setPower(pivot + (-vertical + horizontal));
     }
+    @Override
+    public void init() {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void init_loop(){
+
+    }
+
+    @Override
+    public void loop(){
+
+    }
+}
 
     @Override
     public void init() {
@@ -49,8 +59,3 @@ public class MecanumDriveGrace extends OpMode {
     }
 }
 
-
-
-
-
-}
