c888acd43f4dc7466f1d3f1f7e1e5598d98f635e
Grace <dug29contacts@gmail.com>
2023-08-10 13:38:58 -0400

    made my program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveGrace.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,27 +8,17 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class MecanumDriveGrace extends OpMode {
+
     DcMotor RFMotor;
     DcMotor LFMotor;
     DcMotor RBMotor;
     DcMotor LBMotor;
 
-    public void moveDriveTrain() {
-        double vertical = 0;
-        double horizontal = 0;
-        double pivot = 0;
+    public float speedMultiplier = 0.5f;
 
-        vertical = -gamepad1.left_stick_y;
-        horizontal = gamepad1.left_stick_x;
-        pivot = gamepad1.right_stick_x;
-
-        RFMotor.setPower(pivot + (-vertical + horizontal));
-        RBMotor.setPower(pivot + (-vertical - horizontal));
-        LFMotor.setPower(pivot + (-vertical - horizontal));
-        RFMotor.setPower(pivot + (-vertical + horizontal));
-    }
     @Override
     public void init() {
+
         RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
         LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
         RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
@@ -38,14 +29,37 @@ public class MecanumDriveGrace extends OpMode {
     }
 
     @Override
-    public void init_loop(){
-
+    public void loop(){ //this is the loop that repeats while the program is running
+        moveDriveTrain();
     }
 
-    @Override
-    public void loop(){
+    public void moveDriveTrain() {
+
+
+        double y = gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+
+        //i think right now moving forwards and backwards are correct, but strafing and turning are inverted
+        // (if you try to strafe left, the robot strafes right; if you try to strafe right, the robot strafes left
+        // if you try to turn left, the robot turns right; if you try to turn right, the robot turns left)
+        //change the +s and -s to fix
+        double fl = y + x + rx;
+        double bl = y - x + rx;
+        double fr = y + x - rx;
+        double br = y - x - rx;
+
+
+        LFMotor.setPower(fl*speedMultiplier);
+        LBMotor.setPower(bl*speedMultiplier);
+        RFMotor.setPower(fr*speedMultiplier);
+        RBMotor.setPower(br*speedMultiplier);
 
     }
 }
+
+
+
 
 
