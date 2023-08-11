632f5437ff089173874f75f8f70ea02942619ee9
goofysuzy <suznoda21@gmail.com>
2023-08-11 19:36:49 -0400

    mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideGrace.java
@@ -1,34 +1,4 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+public class LinearSlideGrace {package org.firstinspires.ftc.teamcode;
 
-@TeleOp
-public class LinearSlideGrace extends OpMode {
-
-    DcMotor LiftMotorLeft;
-    DcMotor LiftMotorRight;
-    @Override
-    public void loop() {
-        float speedMultiplier = 0.5f;
-        double y = gamepad2.left_stick_y;
-        LiftMotorRight.setPower(-0.25*y);
-        LiftMotorLeft.setPower(0.25*y);
-        LiftMotorLeft.setTargetPosition();
-    }
-    @Override
-    public void init() {
-        LiftMotorLeft = hardwareMap.get(DcMotor.class, "");
-        LiftMotorRight = hardwareMap.get(DcMotor.class, "");
-
-
-
-
-
-
-    }
-
-
-
-}
