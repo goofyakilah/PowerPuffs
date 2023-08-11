aac7ca052c404cda68bb7c7f21e18b5065b0cc20
goofysuzy <suznoda21@gmail.com>
2023-08-11 17:26:06 -0400

    linear slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideGrace.java
@@ -64,3 +64,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
         }
     }
 }
+
+    public void extendVerticalSlides(int verticalSlideExtendPos) {
+        verticalLeftSlide.setTarget(verticalSlideExtendPos); // the position you want the slides to reach
+        verticalLeftSlide.retMotorEx().setTargetPositionTolerance(1); // set accuracy to 1 tick
+        verticalLeftSlide.toPosition();
+        verticalLeftSlide.setPower(1); // raise at some power
+
+        verticalRightSlide.setTarget(verticalSlideExtendPos);
+        verticalRightSlide.retMotorEx().setTargetPositionTolerance(1);
+        verticalRightSlide.toPosition();
+        verticalRightSlide.setPower(1);
+    }
