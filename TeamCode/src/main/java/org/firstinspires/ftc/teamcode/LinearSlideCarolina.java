ba45fee54fba6723b083d57b3cc2d976819d2ae8
carolinasolana <carolina.solana@icloud.com>
2023-08-11 21:31:48 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideCarolina.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class LinearSlideCarolina extends OpMode {
+    DcMotor liftMotorL;
+    DcMotor liftMotorR;
+    double y = gamepad1.left_stick_y;
+    public float speedLimiter = 0.25f;
+    public void init() {
+
+        liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
+        liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+    }
+        @Override
+        public void loop() {liftArmHigh();}
+    public void liftArmHigh(){
+        liftMotorL.setPower(speedLimiter * y);
+    }
+
+    }
