e9cfdf07fb2e7258933b9cfb0f32a8c4f1a80ab9
alan <300826alan@gmail.com>
2023-08-13 11:22:42 -0400

    MecanumDriveAlan.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideAlan.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class LinearSlideAlan extends OpMode{
+
+    DcMotor liftMotorL;
+    DcMotor liftMotorR;
+
+    public double speedLimiter = 0.5;
+    public double power = 0.5;
+
+    public int[] values = {0, 10, 20, 30};
+    public int currentIndex = 0;
+    public int position = 0;
+
+    public int GROUND = 0;
+    public int LOW = 0;
+    public int MID = 0;
+    public int HIGH = 0;
+
+
+    @Override
+    public void init() {
+        liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
+        liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+
+        liftMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //reverse a motor
+    }
+    @Override
+    public void loop() {
+        liftController(gamepad1.a, gamepad1.b);
+        setGround(gamepad1.a);
+        setLow(gamepad1.x);
+        setMID(gamepad1.b);
+        setHIGH(gamepad1.y);
+    }
+
+    public void setGround(boolean keybind){
+        if(keybind) {
+            moveLift(GROUND);
+        }
+    }
+    public void setLow(boolean keybind){
+        if(keybind) {
+            moveLift(LOW);
+        }
+    }
+    public void setMID(boolean keybind){
+        if(keybind) {
+            moveLift(MID);
+        }
+    }
+    public void setHIGH(boolean keybind){
+        if(keybind) {
+            moveLift(HIGH);
+        }
+    }
+
+    public void liftController(boolean up, boolean down){
+        if(up)
+        {
+            currentIndex = (currentIndex + 1) % values.length;
+        }
+        if(down)
+        {
+            currentIndex = (currentIndex - 1 + values.length) % values.length;
+        }
+        position = values[currentIndex];
+        moveLift(position);
+    }
+
+    public void moveLift(int position){
+
+        int currentPositionL = liftMotorL.getCurrentPosition();
+        int positionDifferenceL = Math.abs(currentPositionL - position);
+
+//        double powerModifier = Math.min(1.0, positionDifferenceL / 100.0);
+
+        liftMotorL.setTargetPosition(position);
+        liftMotorR.setTargetPosition(position);
+        if(positionDifferenceL < 10){
+            liftMotorR.setPower(0);
+            liftMotorL.setPower(0);
+        } else {
+            liftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftMotorR.setPower(power*speedLimiter/*powerModifier*/);
+            liftMotorL.setPower(power*speedLimiter/*powerModifier*/);
+        }
+    }
+}
