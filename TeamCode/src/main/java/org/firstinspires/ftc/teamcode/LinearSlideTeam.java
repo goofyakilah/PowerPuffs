3a7ad93ff8827884070bd23ceb777b990e4b16a3
carolinasolana <carolina.solana@icloud.com>
2023-08-13 12:25:40 -0400

    Merge remote-tracking branch 'origin/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTeam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTeam.java
@@ -14,14 +14,18 @@ public class LinearSlideTeam extends OpMode {
     public void init() {
         liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
         liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+        int positionL = liftMotorL.getCurrentPosition();
+        int positionR = liftMotorR.getCurrentPosition();
 
+        liftMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        liftMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
     }
     @Override
     public void loop() {liftArmHigh();}
 
     public void liftArmHigh(){
         liftMotorL.setPower(speedLimiter * y);
-        liftMotorR.setPower(speedLimiter * -y);
+        liftMotorR.setPower(speedLimiter * y);
         //CHECK WHICH MOTOR IS NEGATIVE AND WHICH IS POSITIVE!!!!
         //stop motors when ---- rotations r made by motor?
     }
