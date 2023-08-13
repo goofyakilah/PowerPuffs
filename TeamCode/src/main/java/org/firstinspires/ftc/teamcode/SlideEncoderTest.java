487d5af2743f711ba286b06175bf2d6505234378
AlanMalan <300826alan@gmail.com>
2023-08-13 11:49:31 -0400

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideEncoderTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class SlideEncoderTest extends OpMode {
+    DcMotor liftMotorL;
+    DcMotor liftMotorR;
+
+    @Override
+    public void init() {
+        liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
+        liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+
+        liftMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //reverse a motor
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("encoderLeft", liftMotorL.getCurrentPosition());
+        telemetry.addData("encoderLeft", liftMotorL.getCurrentPosition());
+        telemetry.update();
+    }
+}
