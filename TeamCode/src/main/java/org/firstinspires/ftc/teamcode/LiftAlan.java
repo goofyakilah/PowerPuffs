3a105053bc93c06be6687ed22b6da2ea05413dfc
Akiulah <akilah.yu@gmail.com>
2023-08-13 14:28:12 -0400

    LiftAlan.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftAlan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftAlan.java
@@ -11,8 +11,7 @@ public class LiftAlan extends OpMode {
     DcMotor liftMotorL;
     DcMotor liftMotorR;
 
-    int targetPositionRight;
-    int targetPositionLeft;
+    int targetPosition;
     boolean isMoving = false;
 
     @Override
@@ -27,31 +26,12 @@ public class LiftAlan extends OpMode {
         liftMotorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         liftMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
-//        liftMotorL.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
     @Override
     public void loop(){
-        if(gamepad1.a && !isMoving) {
-            targetPositionRight = 1000;
-            targetPositionLeft = 1000;
-            liftMotorL.setTargetPosition(targetPositionLeft);
-            liftMotorR.setTargetPosition(targetPositionRight);
-            liftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            liftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            liftMotorR.setPower(0.2);
-            liftMotorL.setPower(-0.2);
-            isMoving = true;
-        }
 
-        if(isMoving && !liftMotorL.isBusy() && !liftMotorR.isBusy()) {
-            liftMotorL.setPower(0);
-            liftMotorR.setPower(0);
-            isMoving = false;
-        }
 
-        telemetry.addData("LEFT", liftMotorL.getCurrentPosition());
-        telemetry.addData("RIGHT", liftMotorR.getCurrentPosition());
-        telemetry.update();
+
     }
 }
