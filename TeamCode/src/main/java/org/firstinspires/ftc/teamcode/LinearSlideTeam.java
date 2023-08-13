e3c91a05619af583a30f495f2d0be50b3e7fefab
Grace <dug29contacts@gmail.com>
2023-08-13 12:12:34 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTeam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideTeam.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
-public class LinearSlideCarolina extends OpMode {
+public class LinearSlideTeam extends OpMode {
     DcMotor liftMotorL;
     DcMotor liftMotorR;
     double y = gamepad1.left_stick_y;
