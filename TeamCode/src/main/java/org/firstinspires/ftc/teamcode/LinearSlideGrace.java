9cb03e86cb87d6911493d16e74a94597981072bf
Grace <dug29contacts@gmail.com>
2023-08-13 12:39:45 -0400

    jiihuotyf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideGrace.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@TeleOp
+public class LinearSlideGrace extends OpMode {
+    DcMotor liftMotorL;
+    DcMotor liftMotorR;
+    double y = gamepad1.left_stick_y;
+    public float speedLimiter = 0.5f;
+
+    @Override
+    public void init() {
+        liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
+        liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+        int positionL = liftMotorL.getCurrentPosition();
+        int positionR = liftMotorR.getCurrentPosition();
+
+        liftMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        liftMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+    }
+    @Override
+    public void loop() {liftArmHigh();}
+
+    public void liftArmHigh(){
+        liftMotorL.setPower(speedLimiter * y);
+        liftMotorR.setPower(speedLimiter * y);
+
+
+    }
+
+}
