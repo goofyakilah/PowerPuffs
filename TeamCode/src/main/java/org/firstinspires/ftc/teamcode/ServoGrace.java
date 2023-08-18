88b157ca95e1fd603846cda2ad376d17d0698d4a
Grace <dug29contacts@gmail.com>
2023-08-18 17:52:33 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
@@ -1,29 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-
+@TeleOp
 public class ServoGrace extends OpMode {
 
-    public Servo servo;
+    public Servo servo1;
+//    public Servo servo2;
+//    boolean move = false;
 
 
     @Override
     public void init() {
-        servo = hardwareMap.get(Servo.class, "Servo");
-        servo.setDirection(Servo.Direction.REVERSE);
+        servo1 = hardwareMap.get(Servo.class, "Claw");
+        servo1.setDirection(Servo.Direction.REVERSE);
+//        servo2 = hardwareMap.get(Servo.class, "Servo2");
+//        servo2.setDirection(Servo.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
         if (gamepad1.left_trigger > 0.3) {
-            servo.setPosition(0.5);}
+            servo1.setPosition(0.5);}
 
         if (gamepad1.right_trigger > 0.3) {
-            servo.setPosition(0.3);}
+            servo1.setPosition(0.3);}
 
-        if
+//        if (gamepad1.a && !move){
+//            servo2.setPosition(0.3);}
+//
+//        if (gamepad1.a && !move){
+//            servo2.setPosition(0.3);}
     }
 
     }
