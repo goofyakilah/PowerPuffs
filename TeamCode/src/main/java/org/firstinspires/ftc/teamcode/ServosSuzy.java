9634faaa230a0586a260fb1ae4bb47a704e91bbe
suzy <suznoda21@gmail.com>
2023-08-18 17:48:36 -0400

    servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServosSuzy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServosSuzy.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
 public class ServosSuzy extends OpMode {
 
     public Servo servo;
@@ -17,6 +20,10 @@ public class ServosSuzy extends OpMode {
 
     if (gamepad1.left_trigger > 0.3) {
         servo.setPosition(0.5);
+    }
+
+    if (gamepad1.right_trigger > 0.3) {
+        servo.setPosition(0.2);
     }
     }
 }
