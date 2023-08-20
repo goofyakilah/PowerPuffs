84488688b81487f49f3895de9ea3518f7f5fb641
Grace <dug29contacts@gmail.com>
2023-08-20 16:28:08 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChatGPTLinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChatGPTLinearSlide.java
@@ -2,63 +2,72 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "LinearSlideControl", group = "TeleOp")
-public class LinearSlideControl extends LinearOpMode {
+@TeleOp
+public class ChatGPTLinearSlide extends LinearOpMode {
 
-    private DcMotor linearSlideMotor;
+    DcMotor LiftMotorL;
+    DcMotor LiftMotorR;
 
     @Override
     public void runOpMode() {
-        linearSlideMotor = hardwareMap.get(DcMotor.class, "linear_slide_motor");
+        LiftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL");
+        LiftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
+
+        int positionL = LiftMotorL.getCurrentPosition();
+        int positionR = LiftMotorR.getCurrentPosition();
+
+        LiftMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        LiftMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+
+        LiftMotorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LiftMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        LiftMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while (opModeIsActive()) {
             double linearSlidePower = gamepad1.left_stick_y;
 
-            // Control the continuous movement of the linear slide using the left stick Y
-            linearSlideMotor.setPower(linearSlidePower);
+            LiftMotorR.setPower(linearSlidePower);
+            LiftMotorL.setPower(linearSlidePower);
 
-            // Control the different heights using buttons A, Y, B, X
+
             if (gamepad1.a) {
-                // Move to the A height position
-                // Adjust motor power and target position accordingly
-                linearSlideMotor.setPower(0.5);
-                // Set the target position for the A height
-                linearSlideMotor.setTargetPosition(/* A height position */);
-                // Use motor run to position mode
-                linearSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorR.setPower(0.2);
+                LiftMotorR.setTargetPosition(0);
+                LiftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorL.setPower(0.2);
+                LiftMotorL.setTargetPosition(0);
+                LiftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             } else if (gamepad1.y) {
-                // Move to the Y height position
-                // Adjust motor power and target position accordingly
-                linearSlideMotor.setPower(0.5);
-                // Set the target position for the Y height
-                linearSlideMotor.setTargetPosition(/* Y height position */);
-                // Use motor run to position mode
-                linearSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorR.setPower(0.2);
+                LiftMotorR.setTargetPosition(500);
+                LiftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorL.setPower(0.2);
+                LiftMotorL.setTargetPosition(500);
+                LiftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             } else if (gamepad1.b) {
-                // Move to the B height position
-                // Adjust motor power and target position accordingly
-                linearSlideMotor.setPower(0.5);
-                // Set the target position for the B height
-                linearSlideMotor.setTargetPosition(/* B height position */);
-                // Use motor run to position mode
-                linearSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorR.setPower(0.2);
+                LiftMotorR.setTargetPosition(700);
+                LiftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorL.setPower(0.2);
+                LiftMotorL.setTargetPosition(700);
+                LiftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             } else if (gamepad1.x) {
-                // Move to the X height position
-                // Adjust motor power and target position accordingly
-                linearSlideMotor.setPower(0.5);
-                // Set the target position for the X height
-                linearSlideMotor.setTargetPosition(/* X height position */);
-                // Use motor run to position mode
-                linearSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorR.setPower(0.2);
+                LiftMotorR.setTargetPosition(1000);
+                LiftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                LiftMotorL.setPower(0.2);
+                LiftMotorL.setTargetPosition(1000);
+                LiftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             } else {
-                // Stop the motor if no buttons are pressed
-                linearSlideMotor.setPower(0);
+                LiftMotorR.setPower(0);
+                LiftMotorL.setPower(0);
             }
 
-            // Update telemetry or other necessary operations
             telemetry.update();
         }
     }
