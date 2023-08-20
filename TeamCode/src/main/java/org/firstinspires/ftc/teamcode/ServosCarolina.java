7459a6ad582e9d841ef520ffecc7442ef3e6d510
carolinasolana <carolina.solana@icloud.com>
2023-08-20 13:31:02 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServosCarolina.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServosCarolina extends OpMode{
+    @Override
+    public void init() {
+        public Servo servo;
+        Claw = hardwareMap.get(Servo.class,"Claw");
+        Servo= hardwareMap.get(Servo.class,"Arm");
+
+    }
+
+    @Override
+    public void loop() {
+if (gamepad1.left_trigger > 0.`
+    }
+}
+
