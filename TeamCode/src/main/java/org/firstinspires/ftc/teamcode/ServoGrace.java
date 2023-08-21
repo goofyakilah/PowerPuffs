9d42a8830bfa28d5df3758f185c1aa0375703303
david <epdu@temple.edu>
2023-08-21 19:24:31 -0400

    Merge remote-tracking branch 'origin/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
@@ -7,17 +7,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class ServoGrace extends OpMode {
 
-    public Servo servo1;
-//    public Servo servo2;
+//    public Servo servo1;
+    public Servo servo2;
 //    public Servo servo3;
 //    public Servo servo4;
-//    boolean move = false;
+    boolean move = false;
 
     @Override
     public void init() {
-        servo1 = hardwareMap.get(Servo.class, "Claw");
+//        servo1 = hardwareMap.get(Servo.class, "Claw");
 //        servo1.setDirection(Servo.Direction.REVERSE);
-//        servo2 = hardwareMap.get(Servo.class, "Servo2");
+        servo2 = hardwareMap.get(Servo.class, "Claw");
 //        servo2.setDirection(Servo.Direction.REVERSE);
 //        servo3 = hardwareMap.get(Servo.class, "Servo3");
 ////      servo2.setDirection(Servo.Direction.REVERSE);
@@ -28,14 +28,16 @@ public class ServoGrace extends OpMode {
 
     @Override
     public void loop() {
-        if (gamepad1.left_trigger > 0.3) {
-            servo1.setPosition(0);}
+//        if (gamepad1.left_trigger > 0.3) {
+//            servo1.setPosition(0.15);}
+//
+//        if (gamepad1.right_trigger > 0.3) {
+////            servo1.setPosition(0.38); //max
+//            servo1.setPosition(0.35);
+//            }
 
-        if (gamepad1.right_trigger > 0.3) {
-            servo1.setPosition(0.2);}
-
-//        if (gamepad1.a && !move){
-//            servo2.setPosition(0.3);}
+        if (gamepad1.a && !move){
+            servo2.setPosition(0.3);}
 //
 //        if (gamepad1.a && !move){
 //            servo2.setPosition(0.3);}
