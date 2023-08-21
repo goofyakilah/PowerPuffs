3ae5778b80f6945d62291ed2d79b3c8fdcdbe5dc
Grace <dug29contacts@gmail.com>
2023-08-21 18:36:50 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
@@ -9,15 +9,21 @@ public class ServoGrace extends OpMode {
 
     public Servo servo1;
 //    public Servo servo2;
+//    public Servo servo3;
+//    public Servo servo4;
 //    boolean move = false;
 
 
     @Override
     public void init() {
         servo1 = hardwareMap.get(Servo.class, "Claw");
-        servo1.setDirection(Servo.Direction.REVERSE);
+//        servo1.setDirection(Servo.Direction.REVERSE);
 //        servo2 = hardwareMap.get(Servo.class, "Servo2");
 //        servo2.setDirection(Servo.Direction.REVERSE);
+//        servo3 = hardwareMap.get(Servo.class, "Servo3");
+////      servo2.setDirection(Servo.Direction.REVERSE);
+//        servo3 = hardwareMap.get(Servo.class, "Servo3");
+
     }
 
     @Override
@@ -33,6 +39,10 @@ public class ServoGrace extends OpMode {
 //
 //        if (gamepad1.a && !move){
 //            servo2.setPosition(0.3);}
+
+//        if (gamepad1.b && !move){
+//            servo3.setPosition(0.3);
+//            servo4.setPosition(0.3);}
     }
 
     }
