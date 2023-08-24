9fc5e41e84b83345f33417e68d4a43fa0f2af992
david <epdu@temple.edu>
2023-08-24 16:13:59 -0400

    Merge remote-tracking branch 'origin/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerpuffV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerpuffV1.java
@@ -1,11 +1,9 @@
+//Mecanum+Slides tested
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
-
 @TeleOp
 public class PowerpuffV1 extends OpMode {
 
@@ -15,9 +13,7 @@ public class PowerpuffV1 extends OpMode {
         DcMotor LBMotor; // Mecanum drive motors
         DcMotor liftMotorL; // linear slides motors
         DcMotor liftMotorR; // linear slides motors
-
         boolean move = false;
-
         private static final double SLIDE_POWER = 0.9; // linear slides Adjust as needed
         private static final int POSITION_A = 1000;   // linear slides Adjust these positions as needed
         private static final int POSITION_B = 2000; // linear slides
@@ -32,23 +28,17 @@ public class PowerpuffV1 extends OpMode {
             LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
             RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
             LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
-
             RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
             RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-
             liftMotorL = hardwareMap.get(DcMotor.class, "liftMotorL"); // linear slides
             liftMotorR = hardwareMap.get(DcMotor.class, "liftMotorR");
-
             int positionL = liftMotorL.getCurrentPosition();
             int positionR = liftMotorR.getCurrentPosition();
-
             liftMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
             liftMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
-
             liftMotorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             liftMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
             liftMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
         }
 
@@ -56,17 +46,16 @@ public class PowerpuffV1 extends OpMode {
         public void loop(){
 
             moveDriveTrain();
-
             if (gamepad1.a && !move) {
-                moveSlideToPosition(POSITION_A); // linear slides
+                moveSlideToPosition(POSITION_A);
             }  else if (gamepad1.b && !move) {
-                moveSlideToPosition(POSITION_B); // linear slides
+                moveSlideToPosition(POSITION_B);
             }   else if (gamepad1.y && !move) {
-                moveSlideToPosition(POSITION_Y); // linear slides
+                moveSlideToPosition(POSITION_Y);
             }   else if (gamepad1.x && !move) {
-                moveSlideToPosition(POSITION_X); // linear slides
+                moveSlideToPosition(POSITION_X);
             }   else  {
-                liftArmHigh(); // linear slides
+                liftArmHigh();
             }
 
         }
@@ -75,7 +64,6 @@ public class PowerpuffV1 extends OpMode {
             double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
-
 
             double fl = y + x + rx;
             double bl = y - x + rx;
@@ -94,35 +82,32 @@ public class PowerpuffV1 extends OpMode {
             telemetry.addData("fr",fr);
             telemetry.addData("br",br);
             telemetry.update();
+        }
+        private void moveSlideToPosition(int targetPosition) {
+            liftMotorL.setTargetPosition(targetPosition);
+            liftMotorR.setTargetPosition(targetPosition);
+            liftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            liftMotorR.setPower(SLIDE_POWER);
+            liftMotorL.setPower(SLIDE_POWER);
+            move=true;
+            while (liftMotorL.isBusy() && liftMotorR.isBusy() && move) {
+                // Wait until the motor reaches the target position
+            }
+            liftMotorL.setPower(0);
+            liftMotorR.setPower(0);
+            liftMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            liftMotorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            move=false;
+        }
+
+        public void liftArmHigh(){
+            double y = - gamepad1.right_stick_y;
+            liftMotorL.setPower(speedLimiter * y);
+            liftMotorR.setPower(speedLimiter * y);
 
         }
 
-    private void moveSlideToPosition(int targetPosition) {
-        liftMotorL.setTargetPosition(targetPosition);
-        liftMotorL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotorR.setTargetPosition(targetPosition);
-        liftMotorR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        liftMotorR.setPower(SLIDE_POWER);
-        liftMotorL.setPower(SLIDE_POWER);
-        move=true;
-        while (liftMotorL.isBusy() && liftMotorR.isBusy() && move) {
-            // Wait until the motor reaches the target position
-        }
-
-        liftMotorL.setPower(0);
-        liftMotorR.setPower(0);
-        liftMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        liftMotorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        move=false;
-    }
-
-
-    public void liftArmHigh(){
-        double y = - gamepad1.left_stick_y;
-        liftMotorL.setPower(speedLimiter * y);
-        liftMotorR.setPower(speedLimiter * y);
-
-    }
 
 
 }
