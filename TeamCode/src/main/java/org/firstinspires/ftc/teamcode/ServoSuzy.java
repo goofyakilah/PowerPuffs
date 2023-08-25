f4efe958a5373032d99cbf3541ee17f7bc080dcd
suzy <suznoda21@gmail.com>
2023-08-25 15:43:08 -0400

    servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoSuzy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoSuzy.java
@@ -6,21 +6,30 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class ServoSuzy extends OpMode {
-    public Servo servo;
+//    public Servo servo1;
+    public Servo servo2;
+    boolean push = false;
     @Override
     public void init() {
-        servo = hardwareMap.get(Servo.class,"Claw");
+//        servo1 = hardwareMap.get(Servo.class,"Claw");
+        servo2 = hardwareMap.get(Servo.class,"Wrist");
     }
 
     @Override
     public void loop() {
-
-    if (gamepad1.left_trigger > 0.3) {
-        servo.setPosition(0.35);
+//
+//    if (gamepad1.left_trigger > 0.3) {
+//        servo1.setPosition(0.35);
+//    }
+//
+//    if (gamepad1.right_trigger > 0.3) {
+//        servo1.setPosition(0.15);
+//    }
+    if (gamepad1.a && !push) {
+        servo2.setPosition(0);
     }
-
-    if (gamepad1.right_trigger > 0.3) {
-        servo.setPosition(0.15);
+    if (gamepad1.b && !push) {
+        servo2.setPosition(-0.7);
     }
     }
 }
