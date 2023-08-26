477f526596b8541aaa4e6122031698d666de1a1d
Grace <dug29contacts@gmail.com>
2023-08-25 21:09:40 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGrace.java
@@ -9,17 +9,17 @@ public class ServoGrace extends OpMode {
 
     public Servo servo1;
     public Servo servo2;
-//    public Servo servo3;
-//    public Servo servo4;
+    public Servo servo3;
+    public Servo servo4;
     boolean move = false;
 
     @Override
     public void init() {
         servo1 = hardwareMap.get(Servo.class, "Claw");
         servo2 = hardwareMap.get(Servo.class, "wrist");
-//        servo4 = hardwareMap.get(Servo.class, "arm1");
-//        servo3.setDirection(Servo.Direction.REVERSE);
-//        servo3 = hardwareMap.get(Servo.class, "arm2");
+        servo4 = hardwareMap.get(Servo.class, "arm1");
+        servo3.setDirection(Servo.Direction.REVERSE);
+        servo3 = hardwareMap.get(Servo.class, "arm2");
 
     }
 
@@ -31,7 +31,7 @@ public class ServoGrace extends OpMode {
             servo1.setPosition(0.15);
 
         } if (gamepad1.right_trigger > 0.3) {
-            servo1.setPosition(0.38); //max
+//            servo1.setPosition(0.38); //max
             servo1.setPosition(0.35);}
 
          if (gamepad1.a && !move) {
@@ -40,15 +40,15 @@ public class ServoGrace extends OpMode {
         } if (gamepad1.y && !move) {
             servo2.setPosition(0.9);}
 
-//         if (gamepad1.b && !move) {
-//            servo3.setPosition(0.3);
-//            servo4.setPosition(0.3);
-//
-//        } if (gamepad1.x && !move) {
-//            servo3.setPosition(0);
-//            servo4.setPosition(0);
-//
-//        }
+         if (gamepad1.b && !move) {
+            servo3.setPosition(0.4);
+            servo4.setPosition(0.4);
+
+        } if (gamepad1.x && !move) {
+            servo3.setPosition(0);
+            servo4.setPosition(0);
+
+        }
 
 
         }
