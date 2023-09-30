a501ffe7b93b3c8de94d6c9549d38193bc7a18da
Grace <dug29contacts@gmail.com>
2023-09-30 18:33:27 -0400

    jiihuotyf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntakeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntakeTesting.java
@@ -2,7 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class ActiveIntakeTesting extends OpMode {
+    DcMotor Intake;
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
+
 }
