4272b34af42d5a8300255a6ccad75c8080c4f596
carolinasolana <carolina.solana@icloud.com>
2023-09-30 18:49:11 -0400

    HELLO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntakeTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActiveIntakeTesting.java
@@ -3,23 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class ActiveIntakeTesting extends OpMode {
-    DcMotor Intake;
-
-
+    DcMotor IntakeMotor;
+    public double SpeedLimiter = 0.5;
     @Override
     public void init() {
 
+    IntakeMotor= hardwareMap.get(DcMotor.class,"IntakeMotor");
     }
 
     @Override
     public void loop() {
-
+    double leftTrigger = gamepad1.left_trigger;
+    double y = gamepad1.right_stick_y;
+    IntakeMotor.setPower(SpeedLimiter * y);
     }
 }
 
 
-}
+
