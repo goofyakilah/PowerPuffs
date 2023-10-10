cd69525fdb9cdd52b1e62a534d208ad236b9bc4f
AlanMalan <300826alan@gmail.com>
2023-10-10 19:59:16 -0400

    test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SAMPLEAUTONOMOUS.java
@@ -0,0 +1,192 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Range;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous
+public class SAMPLEAUTONOMOUS extends LinearOpMode {
+
+
+    //declare motors and servos
+    public DcMotorEx sampleMotor;
+
+    public Servo sampleServo;
+
+
+    //start of april tag stuff
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    // Tag ID 1,2,3 from the 36h11 family
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
+    private TrajectorySequence trajFinal;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+        //roadrunner stuff
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        //vision
+        //servo initialization
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+
+        //name motors and servos
+        sampleMotor = hardwareMap.get(DcMotorEx.class, "samplemotor");
+        sampleServo = hardwareMap.get(Servo.class, "sampleservo");
+
+
+        //trajectories are from roadrunner they allow robot to move
+        // learn more at https://learnroadrunner.com/
+        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(startPose)
+                .build();
+
+
+
+        // more april tag stuff
+        while (!isStarted() && !isStopRequested()) {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if (currentDetections.size() != 0) {
+                boolean tagFound = false;
+
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT) {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if (tagFound) {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                } else {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if (tagOfInterest == null) {
+                        telemetry.addLine("(The tag has never been seen)");
+                    } else {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    }
+                }
+
+            } else {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if (tagOfInterest == null) {
+                    telemetry.addLine("(The tag has never been seen)");
+                } else {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+
+
+            if (tagOfInterest != null) {
+                telemetry.addLine("Tag snapshot:\n");
+                tagToTelemetry(tagOfInterest);
+                telemetry.update();
+            } else {
+                telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+                telemetry.update();
+            }
+
+
+            // this sets the final trajectory where we park in the spot based on what the april tag was
+            // at the beginning of the game
+            if (tagOfInterest == null || tagOfInterest.id == LEFT) {
+                trajfinal = drive.trajectorySequenceBuilder(traj6.end())
+
+                        .build();
+            } else if (tagOfInterest.id == MIDDLE) {
+                trajFinal = drive.trajectorySequenceBuilder(traj6.end())
+
+                        .build();
+            } else {
+                trajFinal = drive.trajectorySequenceBuilder(traj6.end())
+
+
+                        .build();
+            }
+        }
+
+        if (opModeIsActive()){
+            //basically the loop function in teleop
+            sampleFunction();
+
+        }
+    }
+
+    //functions are here
+
+    public void sampleFunction() {
+        //do stuff
+    }
+
+}
