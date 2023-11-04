5ce66431fc4f46caadd9d4fa853e67b6716d1f5d
carolinasolana <carolina.solana@icloud.com>
2023-11-04 18:56:00 -0400

    HELLO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrewingWAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScrewingWAutonomous.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ScrewingWAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous
+public class ScrewingWAutonomous extends LinearOpMode {
+
+    DcMotor RFMotor;
+    DcMotor LFMotor;
+    DcMotor RBMotor;
+    DcMotor LBMotor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+        LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+        RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+        LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+ waitForStart();
+
+        RFMotor.setPower(1);
+        LFMotor.setPower(1);
+        RBMotor.setPower(1);
+        LBMotor.setPower(1);
+        sleep(1000);
+        RFMotor.setPower(0);
+        LFMotor.setPower(0);
+        RBMotor.setPower(0);
+        LBMotor.setPower(0);
+
+        //test function
+
+
+    }
 }
